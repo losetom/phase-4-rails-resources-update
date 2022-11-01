7ae8294dd7736154351996a4b4934ad56148678d
Mike Loseto <losetom1998@gmail.com>
2022-11-01 11:20:48 -0400

    Completed Assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,13 @@
 class BirdsController < ApplicationController
+  def increment_likes
+    bird = Bird.findy_by(id:params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
 
   # GET /birds
   def index
@@ -22,10 +31,21 @@ class BirdsController < ApplicationController
     end
   end
 
+  # PATCH /birds/1
+
+  def update
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
-
 end
